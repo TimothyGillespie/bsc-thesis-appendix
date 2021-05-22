36c1be26dd01e9a9177b2c61d95301608cced1b9
TimothyGillespie <Timothy.Gillespie@outlook.de>
2021-05-22 02:00:05 +0200

    Mock up some design

--- frontend/src/pages/StructuralInductionProofs.tsx
+++ frontend/src/pages/StructuralInductionProofs.tsx
@@ -5,6 +5,10 @@ import {Button} from "primereact/button";
 import {Panel} from "primereact/panel";
 import { For } from "react-loops";
 import FormulaAnalyzer from "../utils/FormulaAnalyzer/FormulaAnalyzer";
+import {UseFormRegister} from "react-hook-form/dist/types/form";
+import { Dropdown } from 'primereact/dropdown';
+import {Fieldset} from "primereact/fieldset";
+import {Accordion, AccordionTab} from "primereact/accordion";
 
 const StructuralInductionProofs: React.FC = () => {
     const {register, handleSubmit, control} = useForm<FormInputs>();
@@ -14,28 +18,38 @@ const StructuralInductionProofs: React.FC = () => {
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <div className={"p-fluid"}>
-                <Panel header={"Statement"} toggleable>
+                <Fieldset legend={"Statement"} className={"p-mt-2"} toggleable>
                     <span className="p-mt-3 p-field">
                         <label htmlFor="statement">Statement</label>
                         <InputText {...register('statement')}/>
                     </span>
-                </Panel>
-                <Panel header={"Function Definitions"} toggleable>
-                    <FunctionDefinitions control={control}/>
-                </Panel>
-                <Panel header={"Variable Definitions"} toggleable>
-                </Panel>
+                </Fieldset>
+                <Fieldset legend={"Function Definitions"} className={"p-mt-2"} toggleable>
+                    <FunctionDefinitions control={control} register={register}/>
+                </Fieldset>
+                <Fieldset legend={"Variable Definitions"} className={"p-mt-2"} toggleable>
+                </Fieldset>
                 <Button type={"submit"} label={"Submit"}/>
             </div>
         </form>
     );
 };
 
-interface FormInputs {
+type FormInputs = {
     statement: string;
+    functionDefinitions: {
+        name: string;
+        parameterCount: number;
+        parameterTypes: string[];
+        returnType: string;
+    }[],
+    variableDefinitions: {
+       name: string;
+       type: string;
+    }[]
 }
 
-function FunctionDefinitions ({ control }: { control: Control<FormInputs> }) {
+function FunctionDefinitions ({ control, register }: { control: Control<FormInputs>, register: UseFormRegister<FormInputs>}) {
     const statement = useWatch({
         control,
         name: 'statement',
@@ -43,12 +57,32 @@ function FunctionDefinitions ({ control }: { control: Control<FormInputs> }) {
     });
     return (
         <div>
-            <For of={Object.entries(new FormulaAnalyzer(statement).getFunctions())} as={([name, info]) =>
-                <p>{name}: {info.parameterCount}</p>
-            }/>
+            <Accordion>
+                <AccordionTab header={"depth: Binary Tree -> Integer"}>
+                    <div className={"p-grid"}>
+                        <div className={"p-col-5"}>
+                            <Dropdown options={typeDropdownOptions} className={"p-mt-2"}/>
+                            <Dropdown options={typeDropdownOptions} className={"p-mt-2"}/>
+                            <Dropdown options={typeDropdownOptions} className={"p-mt-2"}/>
+                        </div>
+                        <div className={"p-col-2 p-d-flex p-justify-center p-align-center"} style={{fontSize: '33pt'}}>
+                            &#x27FC;
+                        </div>
+                        <div className={"p-col-5"}>
+                            <Dropdown options={typeDropdownOptions} className={"p-mt-2"}/>
+                        </div>
+                    </div>
+                </AccordionTab>
+            </Accordion>
         </div>
 
     );
 }
+
+const typeDropdownOptions = [
+    {label: 'Integer', value: 'Int'},
+    {label: 'Binary Tree', value: 'BTree'},
+    {label: 'Non-Empty Binary Tree', value: 'NEBTree'},
+];
 
 export default StructuralInductionProofs;
