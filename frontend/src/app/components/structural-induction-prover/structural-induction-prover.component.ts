c87374735bf8c0c8b3599a96a0e269220a92e4f6
TimothyGillespie <Timothy.Gillespie@outlook.de>
2021-05-23 14:14:45 +0200

    Add mock up for previous progress

---
+++ frontend/src/app/components/structural-induction-prover/structural-induction-prover.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-structural-induction-prover',
+  templateUrl: './structural-induction-prover.component.html',
+  styleUrls: ['./structural-induction-prover.component.scss']
+})
+export class StructuralInductionProverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
