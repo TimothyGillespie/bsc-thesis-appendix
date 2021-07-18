cedf08f5b21c55e122f03c2ba61a8d3078f256c4
TimothyGillespie <Timothy.Gillespie@outlook.de>
2021-07-18 22:57:35 +0200

    Add PLFormula type in hacky way

--- backend/src/main/kotlin/eu/gillespie/bscthesis/tosmtv20/getSortSpecificConstraints.kt
+++ backend/src/main/kotlin/eu/gillespie/bscthesis/tosmtv20/getSortSpecificConstraints.kt
@@ -15,6 +15,7 @@ fun getSortSpecificConstraints(sort: String, request: ProveStatementRequest, con
     return when(sort) {
         "NAryTree" -> loadNAryTreeConstraints(request, constructorInstantiation)
         "NonEmptyNAryTree" -> loadNonEmptyNAryTreeConstraints(request, constructorInstantiation)
+        "PLFormula" -> loadPLFormulaConstraints(request, constructorInstantiation)
         else -> throw RuntimeException("Unknown sort $sort was given")
     }
 }
@@ -60,4 +61,49 @@ fun loadNAryTreeConstraints(request: ProveStatementRequest, constructorInstantia
 
 fun loadNonEmptyNAryTreeConstraints(request: ProveStatementRequest, constructorInstantiation: Map<String, ConstructorInstantiation>): List<SmtV20TopLevelExpression> {
     return listOf()
+}
+
+fun loadPLFormulaConstraints(request: ProveStatementRequest, constructorInstantiation: Map<String, ConstructorInstantiation>): List<SmtV20TopLevelExpression> {
+    val result = mutableSetOf<StatementTreeVertex>()
+
+    for((_, singleInstantiation) in constructorInstantiation) {
+        if(singleInstantiation.type != "PLForumla")
+            continue
+
+        for(singleParameter in singleInstantiation.parameters)
+            for (singleConstraint in treeConstraints) {
+                result.add(
+                    replaceInTree(
+                        replaceInTree(singleConstraint, "%tree", StatementTreeVertex(singleInstantiation.constructor)),
+                        "%tree_sub",
+                        StatementTreeVertex(singleParameter)
+                    )
+                )
+            }
+    }
+
+    val constructorsSymbols = constructorInstantiation.values.filter { it.type == "PLFormula" }.map { it.constructor }
+    val pairwiseMatch = mutableSetOf<Pair<String, String>>()
+
+    for(i in 0..constructorsSymbols.size - 1) {
+        val firstSymbol = constructorsSymbols.get(i)
+        for(k in i+1..constructorsSymbols.size - 1) {
+            val secondSymbol = constructorsSymbols.get(k)
+            pairwiseMatch.add(Pair(firstSymbol, secondSymbol))
+        }
+    }
+
+    result.addAll(pairwiseMatch.map {
+        StatementTreeVertex(
+            "not",
+            listOf(
+                StatementTreeVertex(
+                    "=",
+                    listOf(StatementTreeVertex(it.first), StatementTreeVertex(it.second))
+                )
+            )
+        )
+    })
+
+    return result.map { SmtV20NamedAssert(null, it) }
 }
