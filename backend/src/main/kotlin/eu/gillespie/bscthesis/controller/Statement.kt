1dc1d95c80c359b8ce6ad2db70f5496550593b85
TimothyGillespie <timothy@gillespie.eu>
2021-09-20 14:47:33 +0200

    Add smt2 route

--- backend/src/main/kotlin/eu/gillespie/bscthesis/controller/Statement.kt
+++ backend/src/main/kotlin/eu/gillespie/bscthesis/controller/Statement.kt
@@ -2,16 +2,22 @@ package eu.gillespie.bscthesis.controller
 
 import eu.gillespie.bscthesis.proveStatement
 import eu.gillespie.bscthesis.model.ProveStatementRequest
+import eu.gillespie.bscthesis.tosmtv20.convertToSmtV20String
 import io.ktor.routing.*
 import io.ktor.application.*
 import io.ktor.request.*
 import io.ktor.response.*
 
 fun Application.statementRoutes() {
-    routing {
+    routing() {
         post("/statement/prove/result") {
             val request = call.receive<ProveStatementRequest>()
             call.respond(proveStatement(request))
+        }
+
+        post("/statement/prove/smt2") {
+            val request = call.receive<ProveStatementRequest>()
+            call.respond(convertToSmtV20String(request))
         }
     }
 }
