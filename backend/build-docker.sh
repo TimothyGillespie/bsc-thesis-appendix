30e45865c68725e5aa60b8da0df3c1a2a91dc1c4
TimothyGillespie <timothy@gillespie.eu>
2021-09-28 18:42:56 +0200

    Update deployment files for usage in GitHub actions

--- backend/build-docker.sh
+++ backend/build-docker.sh
@@ -1,5 +1,4 @@
 VERSION=$1
 IMAGE_NAME=private.docker.gillespie.eu/repository/gillespie-private-docker/bsc-thesis-backend:$VERSION
-mvn clean package
 docker build -t "$IMAGE_NAME" .
 docker push "$IMAGE_NAME"
--- frontend/build-docker.sh
+++ frontend/build-docker.sh
@@ -1,5 +1,4 @@
 VERSION=$1
 IMAGE_NAME=private.docker.gillespie.eu/repository/gillespie-private-docker/bsc-thesis-frontend:$VERSION
-ng build --prod
 docker build -t "$IMAGE_NAME" .
 docker push "$IMAGE_NAME"
